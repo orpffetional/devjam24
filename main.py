a901a683d2b536b6f9447ca03fad992466d2c08a
orpffetional <aditya.shekhar2024@vitstudent.ac.in>
2024-09-20 18:48:21 +0530

    extra files uploded before

---
+++ main.py
@@ -0,0 +1,32 @@
+print("wooff u opened our doors \n smart wardrobe assistant")
+a = int(input('Enter your gender \n 1.male\n 2.female \n~~~~~~'))
+if a == 1:
+    b1= int(input('Enter your Occation \n 1.wedding\n 2.interview\n 3.casual\n~~~~~~'))
+    if b1==1:
+     c1= int(input("enter what advise u want \n 1.choose shirt by your lower \n 2.choose lower by ur shirt \n 3.suggest whole outfit \n~~~~~~ "))
+     if c1==1:
+       f1=int(input("choose your lower colour \n 1.White\n 2.Black\n 3.Gray\n 4.Navy Blue\n 5.Red\n 6.Emerald Green\n 7.Yellow\n 8.Orange\n 9.Turquoise\n 10.Light Pink\n 11.Baby Blue\n 12.Mint Green\n 13.Gold\n 14.Silver\n 15.Brown\n 16.Beige\n 17.Royal Blue\n 18.Olive Green\n 19.Purple\n 20-Coral"))
+     elif c1==2:
+       g1= int(input("choose your shirt colour \n 1.White\n 2.Black\n 3.Gray\n 4.Navy Blue\n 5.Red\n 6.Emerald Green\n 7.Yellow\n 8.Orange\n 9.Turquoise\n 10.Light Pink\n 11.Baby Blue\n 12.Mint Green\n 13.Gold\n 14.Silver\n 15.Brown\n 16.Beige\n 17.Royal Blue\n 18.Olive Green\n 19.Purple\n 20-Coral"))
+     elif c1==3:
+       h1=int(input("enter your skin colour\n 1.fair\n 2.medium\n 3.tan\n 4.dark" ))
+       if h1== 1:
+        print("uff here is a suggetion for u to express ur skintone to another level-  wear a light colur shirt like -- light pink ,navy blue")
+       elif h1==2:
+         print("uff here is a suggetion for u to express ur skintone to another level-  wear a light colur shirt like -- light pink ,navy blue")
+       elif h1==3:
+         print ("uff here is a suggetion for u to express ur skintone to another level-wear dark colur shirt like--black ,maroon ")
+    elif b1 == 2:
+     d1= int(input("enter what advise u want \n 1.choose shirt by your lower \n 2.choose lower by ur shirt \n 3.suggest whole outfit \n~~~~~~ "))
+    elif b1 == 3:
+     e1= int(input("enter what advise u want \n 1.choose shirt by your lower \n 2.choose lower by ur shirt \n 3.suggest whole outfit \n~~~~~~ "))
+elif a==2:
+    b2= int(input('Enter your Occation \n 1. wedding \n 2.interview \n3.casual \n~~~~~~'))
+    if b2==1:
+     c2= int(input("enter what advise u want \n 1.choose shirt by your lower \n 2.choose lower by ur shirt \n 3.suggest whole outfit \n~~~~~~ "))
+    elif b2==2:
+     d2= int(input("enter what advise u want \n 1.choose shirt by your lower \n 2.choose lower by ur shirt \n 3.suggest whole outfit \n~~~~~~ "))
+    elif b2==3:
+     e2= int(input("enter what advise u want \n 1.choose shirt by your lower \n 2.choose lower by ur shirt \n 3.suggest whole outfit \n~~~~~~ "))
+     
+
